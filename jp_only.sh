62dfb4a436fd26e1b5eb463b14161610a3263901
azumew <107566380+azumew@users.noreply.github.com>
2023-09-12 00:27:59 +0900

    Create jp_only.sh

---
+++ jp_only.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+IPTABLES=/sbin/iptables
+wget -q -N http://nami.jp/ipv4bycc/cidr.txt.gz
+gunzip -q -f -c cidr.txt.gz > cidr.txt
+if [ -f cidr.txt ]; then
+    $IPTABLES -F ACCEPT_FILTER
+    sed -n 's/^JP\t//p' cidr.txt | while read address; do
+        $IPTABLES -A ACCEPT_FILTER -s $address -j ACCEPT
+    done
+    $IPTABLES -A ACCEPT_FILTER -j DROP
+fi
